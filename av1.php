efb7fbb186d4d885c686bced7d006b3708c78267
Alexandre <xandeferreiradasilva@gmail.com>
2026-04-13 11:22:05 -0300

    Atualizar o av1.php

--- av1.php
+++ av1.php
@@ -1,5 +1,21 @@
 <?php
 
+    /* O Sr. Water Falls precisa de um sistema de jogo corporativo, para treinar seus gestores em situações difíceis. O jogo deverá gerenciar situações de perguntas e respostas (decisões) encadeadas.
+    O game é composto por vários desafios e cada desafio tem um objetivo específico, como por exemplo, gerenciar o andamento de um projeto, resolver um problema administrativo, contratar um novo funcionário, conceder um empréstimo e outros.
+    Neste primeiro momento será desenvolvido somente o cadastro Usuários, Perguntas e Respostas.
+    Criar as funcionalidades de Criar Perguntas e respostas de multipla escolha, Criar Perguntas e respostas de texto,  alterar Perguntas e suas respostas de multipla escolha, listar todas Perguntas, listar uma Pergunta e excluir Pergunta e respostas.
+    Inicialmente usaremos arquivos texto(txt) para salvar os usuários.
+    As funcionalidades de Perguntas e respostas devem estar disponíveis por tela.
+    O código deverá ser em PHP.
+    Então deverá ser criado:
+    1. Criar Perguntas e respostas de multipla escolha.
+    2.Criar Perguntas e respostas de texto.
+    3. Alterar Perguntas e suas respostas de multipla escolha
+    4. Alterar Perguntas com respostas de texto
+    5. Listar Perguntas e repostas.
+    6. Listar uma Pergunta.
+    7. Excluir Pergunta e respostas*/
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $acao = $_POST["acao"];
 
